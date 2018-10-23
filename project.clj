adf2c60554c409db671113564ec805e7326fa5d2
Peter Strömberg <pez@pezius.com>
2018-10-23 23:33:01 +0200

    First non-SNAPSHOT release

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject pez/clerk "1.0.0-SNAPSHOT"
+(defproject pez/clerk "1.0.0"
   :description "In-page navigation for Single-Page Applications (mimicing Multi-Page Site scrolling)"
   :url "https://github.com/PEZ/clerk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
-                 [org.clojure/core.async "0.4.474"]])
+                 [org.clojure/core.async "0.4.474"]]
+  :deploy-repositories [["clojars" {:sign-releases false
+                                    :url           "https://clojars.org/repo"}]])
